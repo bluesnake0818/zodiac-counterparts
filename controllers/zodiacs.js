12f8a3a395cce99c8935ae538bdecc8e6b66c789
bluesnake0818 <raidient87@gmail.com>
2022-03-07 09:22:58 +0900

    stub up zodiacs/index

--- controllers/zodiacs.js
+++ controllers/zodiacs.js
@@ -1,7 +1,10 @@
 import { Zodiac } from '../models/zodiac.js'
 
 function index(req, res) {
-  console.log("zodiac")
+  // console.log("zodiac")
+  res.render('zodiacs/index', {
+    title: "zodiac"
+  })
 }
 
 export {
